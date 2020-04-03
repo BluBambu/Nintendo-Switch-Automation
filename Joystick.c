77e0583a0b0eb4a980f46c4ca7c52888ba1a4ca7
BluBambu <wenjieshi1@gmail.com>
2020-04-03 16:43:32 -0700

    Serial only needs one character for one command now

--- Joystick.c
+++ Joystick.c
@@ -30,84 +30,60 @@ these buttons for our use.
 uint8_t target = RELEASE;
 uint16_t command;
 
-void parseLine(char *line) {
-	char t[8];
-	char c[16];
-  sscanf(line, "%s %s", t, c);
-	if (strcasecmp(t, "Button") == 0) {
+
+void parseData(char data) {
+	// static const uint8_t stickIndices[] = {
+	// 	LX,
+	// 	LY,
+	// 	RX,
+	// 	RY
+	// };
+
+	static const uint16_t commandIndices[] = {
+		RELEASE,
+		SWITCH_A,
+		SWITCH_B,
+		SWITCH_X,
+		SWITCH_Y,
+		SWITCH_L,
+		SWITCH_R,
+		SWITCH_ZL,
+		SWITCH_ZR,
+		SWITCH_HOME,
+		SWITCH_SELECT,
+		SWITCH_START,
+		SWITCH_LCLICK,
+		SWITCH_RCLICK,
+		SWITCH_CAPTURE,
+		SWITCH_RELEASE,
+		HAT_TOP,
+		HAT_TOP_RIGHT,
+		HAT_RIGHT,
+		HAT_BOTTOM_RIGHT,
+		HAT_BOTTOM,
+		HAT_BOTTOM_LEFT,
+		HAT_LEFT,
+		HAT_TOP_LEFT,
+		HAT_CENTER,
+		STICK_MIN,
+		STICK_MAX,
+		STICK_CENTER,
+	};
+	
+	if (data == 0)
+	{
+		target = RELEASE;
+	}
+	else if (data <= 15)
+	{
 		target = Button;
-	} else if (strcasecmp(t, "LX") == 0) {
-		target = LX;
-	} else if (strcasecmp(t, "LY") == 0) {
-		target = LY;
-	} else if (strcasecmp(t, "RX") == 0) {
-		target = RX;
-	} else if (strcasecmp(t, "RY") == 0) {
-		target = RY;
-	} else if (strcasecmp(t, "HAT") == 0) {
+	}
+	else if (data <= 24)
+	{
 		target = HAT;
-	} else {
-		target = RELEASE;
 	}
-	if (strcasecmp(c, "Y") == 0) {
-		command = SWITCH_Y;
-	} else if (strcasecmp(c, "B") == 0) {
-		command = SWITCH_B;
-	} else if (strcasecmp(c, "A") == 0) {
-		command = SWITCH_A;
-	} else if (strcasecmp(c, "X") == 0) {
-		command = SWITCH_X;
-	} else if (strcasecmp(c, "L") == 0) {
-		command = SWITCH_L;
-	} else if (strcasecmp(c, "R") == 0) {
-		command = SWITCH_R;
-	} else if (strcasecmp(c, "ZL") == 0) {
-		command = SWITCH_ZL;
-	} else if (strcasecmp(c, "ZR") == 0) {
-		command = SWITCH_ZR;
-	} else if (strcasecmp(c, "SELECT") == 0) {
-		command = SWITCH_SELECT;
-	} else if (strcasecmp(c, "START") == 0) {
-		command = SWITCH_START;
-	} else if (strcasecmp(c, "LCLICK") == 0) {
-		command = SWITCH_LCLICK;
-	} else if (strcasecmp(c, "RCLICK") == 0) {
-		command = SWITCH_RCLICK;
-	} else if (strcasecmp(c, "HOME") == 0) {
-		command = SWITCH_HOME;
-	} else if (strcasecmp(c, "CAPTURE") == 0) {
-		command = SWITCH_CAPTURE;
-	} else if (strcasecmp(c, "RELEASE") == 0) {
-		command = SWITCH_RELEASE;
-	} else if (strcasecmp(c, "MIN") == 0) {
-		command = STICK_MIN;
-	} else if (strcasecmp(c, "MAX") == 0) {
-		command = STICK_MAX;
-	} else if (strcasecmp(c, "TOP") == 0) {
-		command = HAT_TOP;
-	} else if (strcasecmp(c, "TOP_RIGHT") == 0) {
-		command = HAT_TOP_RIGHT;
-	} else if (strcasecmp(c, "RIGHT") == 0) {
-		command = HAT_RIGHT;
-	} else if (strcasecmp(c, "BOTTOM_RIGHT") == 0) {
-		command = HAT_BOTTOM_RIGHT;
-	} else if (strcasecmp(c, "BOTTOM") == 0) {
-		command = HAT_BOTTOM;
-	} else if (strcasecmp(c, "BOTTOM_LEFT") == 0) {
-		command = HAT_BOTTOM_LEFT;
-	} else if (strcasecmp(c, "LEFT") == 0) {
-		command = HAT_LEFT;
-	} else if (strcasecmp(c, "TOP_LEFT") == 0) {
-		command = HAT_TOP_LEFT;
-	} else if (strcasecmp(c, "CENTER") == 0) {
-		if (target == HAT) {
-			command = HAT_CENTER;
-		} else {
-			command = STICK_CENTER;
-		}
-	} else {
-		target = RELEASE;
-	}
+	
+	command = commandIndices[data];
 }
 
 #define MAX_BUFFER 32
@@ -118,13 +94,8 @@ ISR(USART1_RX_vect) {
 	if (Serial_IsSendReady()) {
 		printf("%c", c);
 	}
-	if (c == '\r') {
-		parseLine(b);
-		l = 0;
-		memset(b, 0, sizeof(b));
-	} else if (c != '\n' && l < MAX_BUFFER) {
-		b[l++] = c;
-	}
+
+	parseData(c);
 }
 
 // Main entry point.
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,37 +2,65 @@
 
 import serial
 from enum import Enum
-import time
+from time import sleep
+import struct
 
 ser = None
 
-class Button(Enum):
-    A = "A"
-    B = "B"
-    X = "X"
-    Y = "Y"
-    L = "L"
-    R = "R"
-    ZL = "ZL"
-    ZR = "ZR"
-    HOME = "HOME"
+class Special(Enum):
+    RELEASE = "RELEASE"
 
+class Button(Enum):
+    A = "Button A"
+    B = "Button B"
+    X = "Button X"
+    Y = "Button Y"
+    L = "Button L"
+    R = "Button R"
+    ZL = "Button ZL"
+    ZR = "Button ZR"
+    HOME = "Button HOME"
+    SELECT = "Button SELECT"
+    START = "Button START"
+    LCLICK = "Button LCLICK"
+    RCLICK = "Button RCLICK"
+    CAPTURE = "Button CAPTURE"
+    RELEASE = "Button RELEASE"
+
+class Hat(Enum):
+    TOP = "HAT TOP"
+    TOP_RIGHT = "HAT TOP_RIGHT"
+    RIGHT = "HAT RIGHT"
+    BOTTOM_RIGHT = "HAT BOTTOM_RIGHT"
+    BOTTOM = "HAT BOTTOM"
+    BOTTOM_LEFT = "HAT BOTTOM_LEFT"
+    LEFT = "HAT LEFT"
+    TOP_LEFT = "HAT TOP_LEFT"
+    CENTER = "HAT CENTER"
 
 def initialize_serial(port:str):
     global ser
     ser = serial.Serial('COM3', 9600)
 
+def __send_command(data:int):
+    data_binary = struct.pack('>B', data)
+    ser.write(data_binary)
+    msg_char_output = ser.read()
+    print(data_binary)
+    print(msg_char_output)
+    assert data_binary == msg_char_output
 
-def __send_command(msg:str):
-    for msg_char in msg:
-        msg_char_input = f'{msg_char}'.upper().encode('utf-8')
-        ser.write(msg_char_input)
-        msg_char_output = ser.read()
-        assert msg_char_input is msg_char_output
+def press(btn):
+    values = Special._member_names_
+    values.extend(Button._member_names_)
+    values.extend(Hat._member_names_)
 
+    data = values.index(btn.name)
+    __send_command(data)
+    sleep(0.04)
+    __send_command(values.index(Special.RELEASE.name))
 
-def press(btn:Button):
-    __send_command('Button %s\r\n' % btn.value)
-    time.sleep(0.05)
-    __send_command('Release\r\n')
-
+def press_rep(btn, repeat_num:int):
+    for i in range(repeat_num):
+        press(btn)
+        sleep(0.04)
