33bb2e9f61160b8c0e92692288960ca7a48d33c0
BluBambu <wenjieshi1@gmail.com>
2020-04-03 17:49:35 -0700

    Removing useless scripts

--- scripts/animal_crossing_miles.py
+++ scripts/animal_crossing_miles.py
@@ -5,5 +5,12 @@ from utils import *
 def main():
     initialize_serial('COM3')
 
+    // Click through title menu
+    press(Button.A)
+    sleep(5)
+    press(Button.A)
+
+    # increment_date()
+
 if __name__ == "__main__":
     main()
